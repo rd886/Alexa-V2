6495770c099c9229e42f124d2d7378efe6d4e0ad
rd886 <108724560+rd886@users.noreply.github.com>
2022-09-26 22:05:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TG-V4MP1R3/Alexa-V2.git /Alexa-V2
+  git clone https://github.com/rd886/Alexa-V2.git /Alexa-V2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Alexa-V2
 fi
 cd /Alexa-V2
 pip3 install -U -r requirements.txt
-echo "Starting Alexa-v2....🔥"
+echo "Starting Panda....🔥"
 python3 bot.py
